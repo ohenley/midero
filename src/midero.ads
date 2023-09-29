4b28c11bd3b3da09c120ed8843c99b43086742b7
bullydd <clubdeveloppeursenegal@gmail.com>
2023-09-29 20:21:43 +0000

    update midero.ads

--- src/midero.ads
+++ src/midero.ads
@@ -6,7 +6,9 @@
 package Midero is 
 	pragma pure;
 	
-   type Polarity is (ONE, TWO);
+	type Direction is (Forward, Backward);
+   type Power_Level is range 0 .. 100;
+   type Centimeters is range 0 .. 400;
    type Frequency is new Integer range 0 .. 50_000_000;
    type Revolution is new Integer range 0 .. 1_000;
    
