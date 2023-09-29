280ef5619d3faccbb165cc37c183dfe539f32e48
bullydd <clubdeveloppeursenegal@gmail.com>
2023-09-29 20:33:03 +0000

    update systm_config spec

--- src/system_configuration.ads
+++ src/system_configuration.ads
@@ -8,9 +8,11 @@ package System_Configuration is
    --  here for ease of setting with the big picture in view.
 
    Main_Priority           : constant Priority := Priority'First; -- lowest
-   Sonar_Priority          : constant Priority := Main_Priority + 1;
-   Vehicle_Priority        : constant Priority := Sonar_Priority + 1;
-   Scheduler_Priority      : constant Priority := Vehicle_Priority + 1;
+   Scanner_Priority        : constant Priority := Main_Priority + 1;
+   Vehicle_Priority        : constant Priority := Scanner_Priority + 1;
+   Sonar_Priority          : constant Priority := Vehicle_Priority + 1;
+   RF_Priority             : constant Priority := Sonar_Priority + 1;
+   Scheduler_Priority      : constant Priority := RF_Priority + 1;
 
    Highest_Priority : Priority renames Scheduler_Priority;
    --  whichever is highest. All the tasks call into the global initialization
@@ -21,7 +23,10 @@ package System_Configuration is
    --  with the big picture in view. They are merely numbers rather than values
    --  of type Time_Span.
 
-   Sonar_Period            : constant := 250;     -- milliseconds
-   Vehicle_Period          : constant := 150;
+   Scanner_Period          : constant := 2000;    -- seconds
+   Vehicle_Period          : constant := 200;
+   Sonar_Period            : constant := 150;     -- milliseconds
+   RF_Period               : constant := 100;
+
 
 end System_Configuration;
